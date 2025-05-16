3c7822250e8966fb95a27f4e1df51adcc5fcde8c
Nicklas Kevin Frank <nicklas@monori.dk>
2025-05-16 21:47:10 +0200

    Update BookResource.php

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -16,6 +16,7 @@ class BookResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'author' => $this->author,
